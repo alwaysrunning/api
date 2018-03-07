5b4c084a15cd8cd033a148e6bed28fd05a54062b
yanghaitao <178224406@qq.com>
2018-03-07 11:23:43 +0800

    pro dev

--- config/db.js
+++ config/db.js
@@ -1,9 +1,16 @@
 module.exports = {
-    mysql : {
+    pro : {
         host: '96.44.169.189',
         user: 'haitaowa_test',
         password: 'hunanyht287',
         database: 'haitaowa_test',
+        port: 3306
+    },
+    dev : {
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'test',
         port: 3306
     }
 }
--- db/connectMysql.js
+++ db/connectMysql.js
@@ -1,5 +1,16 @@
 var mysql = require('mysql');
 var db = require('../config/db');
-var sql = mysql.createConnection(db.mysql)
+var sql;
+var arguments = process.argv.splice(2);
+
+var args = arguments[0]
+
+if(args && args.indexOf('pro')>-1){
+    process.env.NODE_ENV = 'production'
+    sql = mysql.createConnection(db.prd)
+}else{
+    process.env.NODE_ENV = 'development'
+    sql = mysql.createConnection(db.dev)
+}
 
 module.exports = sql;
